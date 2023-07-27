5b57e56752f48b960341f3a38e8b54e2a57620c7
gthiago <thiago.gonncalves@gmail.com>
2023-07-27 16:28:02 -0300

    alterações no projeto

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gthiago/projetos/turismo/env"
+set -gx VIRTUAL_ENV "/home/gthiago/Documentos/api_turismo/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
